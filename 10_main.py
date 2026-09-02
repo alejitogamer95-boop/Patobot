baedf9f2c60c9387cbd79b66f7cf097694949b7d
alejitogamer95-boop <alejitogamer95@gmail.com>
2026-09-02 01:19:54 -0600

    Delete 10_main.py

--- 10_main.py
+++
@@ -1,8 +0,0 @@
-threading.Thread(target=run_flask_server, daemon=True).start()
-try:
-    gui.root.after(1000, gui.actualizar_dispositivos_spotify)
-except Exception:
-    pass
-
-if __name__ == "__main__":
-    gui.root.mainloop()
